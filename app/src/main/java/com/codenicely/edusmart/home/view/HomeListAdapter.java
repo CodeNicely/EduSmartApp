fa1e8d0c8fc27eeb20458618f99ab26101004b99
Meghal Agrawal - Developer <meghal.nit@gmail.com>
2017-02-04 20:34:16 +0530

    Updated recycler view for home

--- app/src/main/java/com/codenicely/edusmart/home/view/HomeListAdapter.java
+++ app/src/main/java/com/codenicely/edusmart/home/view/HomeListAdapter.java
@@ -23,12 +23,14 @@ import butterknife.BindView;
 public class HomeListAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 
     private List<HomeListDataDetails> homeListDataDetailsList = new ArrayList<>();
-    private static final int CARD_TYPE_SUBJECT =5 ;
-    private static final int CARD_TYPE_NOTICE =1 ;
-    private static final int CARD_TYPE_ANNOUNCEMENTS =2 ;
-    private static final int CARD_TYPE_RESOURCES =3 ;
-    private static final int CARD_TYPE_HEADING =0 ;
-    private static final int CARD_TYPE_ASSIGNMENTS =4 ;
+    private static final int CARD_TYPE_HEADING = 0;
+    private static final int CARD_TYPE_NOTICE = 1;
+    private static final int CARD_TYPE_ANNOUNCEMENTS = 2;
+    private static final int CARD_TYPE_RESOURCES = 3;
+    private static final int CARD_TYPE_ASSIGNMENTS = 4;
+    private static final int CARD_TYPE_SUBJECT = 5;
+    private static final int CARD_TYPE_COURSE = 6;
+
     private Context context;
     private LayoutInflater layoutInflater;
 
@@ -39,38 +41,28 @@ public class HomeListAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolde
 
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        if(viewType==CARD_TYPE_NOTICE)
-        {
+        if (viewType == CARD_TYPE_NOTICE) {
             View view = layoutInflater.inflate(R.layout.notice_item, parent, false);
             return new NoticeViewHolder(view);
-        }
-        else if(viewType==CARD_TYPE_ANNOUNCEMENTS)
-        {
+        } else if (viewType == CARD_TYPE_ANNOUNCEMENTS) {
             View view = layoutInflater.inflate(R.layout.notice_item, parent, false);
             return new AnnouncementsViewHolder(view);
-        }
-        else if(viewType==CARD_TYPE_RESOURCES)
-        {
+        } else if (viewType == CARD_TYPE_RESOURCES) {
             View view = layoutInflater.inflate(R.layout.resources_item, parent, false);
             return new ResourcesViewHolder(view);
-        }
-        else if(viewType==CARD_TYPE_HEADING)
-        {
+        } else if (viewType == CARD_TYPE_HEADING) {
             View view = layoutInflater.inflate(R.layout.heading_item, parent, false);
             return new HeadingViewHolder(view);
-        }
-        else if(viewType==CARD_TYPE_ASSIGNMENTS)
-        {
+        } else if (viewType == CARD_TYPE_ASSIGNMENTS) {
             View view = layoutInflater.inflate(R.layout.assignments_item, parent, false);
             return new AssignmentsViewHolder(view);
-        }
-        else if(viewType==CARD_TYPE_SUBJECT)
-        {
+        } else if (viewType == CARD_TYPE_SUBJECT) {
             View view = layoutInflater.inflate(R.layout.subject_item, parent, false);
             return new SubjectViewHolder(view);
-        }
-        else
-        {
+        } else if (viewType == CARD_TYPE_COURSE) {
+            View view = layoutInflater.inflate(R.layout.subject_item, parent, false);
+            return new SubjectViewHolder(view);
+        } else {
             View view = layoutInflater.inflate(R.layout.empty_item, parent, false);
             return new EmptyViewHolder(view);
         }
@@ -78,32 +70,21 @@ public class HomeListAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolde
 
     @Override
     public int getItemViewType(int position) {
-        if(homeListDataDetailsList.get(position).getCard_type()==CARD_TYPE_NOTICE)
-        {
+        if (homeListDataDetailsList.get(position).getCard_type() == CARD_TYPE_NOTICE) {
             return CARD_TYPE_NOTICE;
-        }
-        else if(homeListDataDetailsList.get(position).getCard_type()==CARD_TYPE_ANNOUNCEMENTS)
-        {
+        } else if (homeListDataDetailsList.get(position).getCard_type() == CARD_TYPE_ANNOUNCEMENTS) {
             return CARD_TYPE_ANNOUNCEMENTS;
-        }
-        else if(homeListDataDetailsList.get(position).getCard_type()==CARD_TYPE_RESOURCES)
-        {
+        } else if (homeListDataDetailsList.get(position).getCard_type() == CARD_TYPE_RESOURCES) {
             return CARD_TYPE_RESOURCES;
-        }
-        else if(homeListDataDetailsList.get(position).getCard_type()==CARD_TYPE_HEADING)
-        {
+        } else if (homeListDataDetailsList.get(position).getCard_type() == CARD_TYPE_HEADING) {
             return CARD_TYPE_HEADING;
-        }
-        else if(homeListDataDetailsList.get(position).getCard_type()==CARD_TYPE_ASSIGNMENTS)
-        {
-            return  CARD_TYPE_ASSIGNMENTS;
-        }
-        else if(homeListDataDetailsList.get(position).getCard_type()==CARD_TYPE_SUBJECT)
-        {
+        } else if (homeListDataDetailsList.get(position).getCard_type() == CARD_TYPE_ASSIGNMENTS) {
+            return CARD_TYPE_ASSIGNMENTS;
+        } else if (homeListDataDetailsList.get(position).getCard_type() == CARD_TYPE_SUBJECT) {
             return CARD_TYPE_SUBJECT;
-        }
-        else
-        {
+        } else if (homeListDataDetailsList.get(position).getCard_type() == CARD_TYPE_COURSE) {
+            return CARD_TYPE_COURSE;
+        } else {
             return -999;
         }
     }
@@ -116,52 +97,32 @@ public class HomeListAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolde
         homeViewHolder.topic_description.setText(homeListDataDetails.getDescription());
         homeListDataDetails = homeListDataDetailsList.get(position);
 
-       if(homeListDataDetails.getCard_type()==CARD_TYPE_HEADING)
-       {
-           HeadingViewHolder headingViewHolder= (HeadingViewHolder)holder;
+        if (homeListDataDetails.getCard_type() == CARD_TYPE_HEADING) {
+            HeadingViewHolder headingViewHolder = (HeadingViewHolder) holder;
 
-       }
-        else if(homeListDataDetails.getCard_type()==CARD_TYPE_RESOURCES)
-       {
-           if(homeListDataDetails.getCard_type()==CARD_TYPE_HEADING)
-           {
-               HeadingViewHolder headingViewHolder= (HeadingViewHolder)holder;
+        } else if (homeListDataDetails.getCard_type() == CARD_TYPE_RESOURCES) {
+            ResourcesViewHolder resourcesViewHolder = (ResourcesViewHolder) holder;
 
-           }
-       }
-        else if(homeListDataDetails.getCard_type()==CARD_TYPE_NOTICE)
-       {
-           if(homeListDataDetails.getCard_type()==CARD_TYPE_HEADING)
-           {
-               HeadingViewHolder headingViewHolder= (HeadingViewHolder)holder;
+        } else if (homeListDataDetails.getCard_type() == CARD_TYPE_NOTICE) {
+            NoticeViewHolder noticeViewHolder = (NoticeViewHolder) holder;
 
-           }
-       }
-        else if(homeListDataDetails.getCard_type()==CARD_TYPE_ASSIGNMENTS)
-       {
-           if(homeListDataDetails.getCard_type()==CARD_TYPE_HEADING)
-           {
-               HeadingViewHolder headingViewHolder= (HeadingViewHolder)holder;
+        } else if (homeListDataDetails.getCard_type() == CARD_TYPE_ASSIGNMENTS) {
+            AssignmentsViewHolder assignmentsViewHolder = (AssignmentsViewHolder) holder;
 
-           }
-       }
-        else if(homeListDataDetails.getCard_type()==CARD_TYPE_SUBJECT)
-       {
+        } else if (homeListDataDetails.getCard_type() == CARD_TYPE_SUBJECT) {
+            SubjectViewHolder subjectViewHolder = (SubjectViewHolder) holder;
 
-       }
-        else if(homeListDataDetails.getCard_type()==CARD_TYPE_ANNOUNCEMENTS)
-       {
-           if(homeListDataDetails.getCard_type()==CARD_TYPE_HEADING)
-           {
-               HeadingViewHolder headingViewHolder= (HeadingViewHolder)holder;
+        } else if (homeListDataDetails.getCard_type() == CARD_TYPE_ANNOUNCEMENTS) {
 
-           }
-       }
-        else
-       {
-               HeadingViewHolder headingViewHolder= (HeadingViewHolder)holder;
+            AnnouncementsViewHolder announcementsViewHolder = (AnnouncementsViewHolder) holder;
 
-       }
+        } else if (homeListDataDetails.getCard_type() == CARD_TYPE_ANNOUNCEMENTS) {
+            CourseViewHolder courseViewHolder = (CourseViewHolder) holder;
+
+        } else {
+            HeadingViewHolder headingViewHolder = (HeadingViewHolder) holder;
+
+        }
     }
 
     @Override
@@ -192,6 +153,7 @@ public class HomeListAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolde
             super(itemView);
         }
     }
+
     private class NoticeViewHolder extends RecyclerView.ViewHolder {
         public NoticeViewHolder(View view) {
             super(view);
@@ -233,5 +195,11 @@ public class HomeListAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolde
             super(view);
         }
     }
+
+    private class CourseViewHolder extends RecyclerView.ViewHolder {
+        public CourseViewHolder(View view) {
+            super(view);
+        }
     }
+}
 
